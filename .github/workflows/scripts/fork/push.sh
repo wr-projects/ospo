d2794274313154c925115974a6cec66bc08d28a7
GregoireF <gfavreau.wrprojects@gmail.com>
2022-12-27 16:23:03 +0100

    test

--- .github/workflows/scripts/fork/push.sh
+++ .github/workflows/scripts/fork/push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Configure git to leave PR comments as the XX service account
-BOT_USER=""
-BOT_EMAIL=""
+BOT_USER="autoforkbot[bot]"
+BOT_EMAIL="121503090+autoforkbot[bot]@users.noreply.github.com"
 
 git config --local user.name ${BOT_USER}
 git config --local user.email ${BOT_EMAIL}
