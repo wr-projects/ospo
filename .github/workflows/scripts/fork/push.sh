b1d3a028cfd74328f2243eb13a83a516231959fa
GregoireF <gfavreau.wrprojects@gmail.com>
2022-12-27 16:06:14 +0100

    update

--- .github/workflows/scripts/fork/push.sh
+++ .github/workflows/scripts/fork/push.sh
@@ -4,7 +4,7 @@
 BOT_USER="autoforkbot[bot]"
 BOT_EMAIL="121503090+autoforkbot[bot]@users.noreply.github.com"
 
-git config --local user.name ${BOT_USER}
-git config --local user.email ${BOT_EMAIL}
+git config --global user.name ${BOT_USER}
+git config --global user.email ${BOT_EMAIL}
 git commit -m "Process ${GITHUB_ACTOR}'s request (PR "${GITHUB_PR})"
 git push
